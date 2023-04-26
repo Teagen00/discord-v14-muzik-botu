02d42a7c12b50bb12ce50c5aa039a4eb2add7aaa
Teagen00 <71625509+Teagen00@users.noreply.github.com>
2023-04-26 16:01:53 +0300

    Update ayarlar.js

--- settings/ayarlar.js
+++ settings/ayarlar.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-    TOKEN: process.env.TOKEN || "TOKEN GİR",  // bot token gir :)
+    TOKEN: process.env.TOKEN || "MTEwMDEyODg3NTAyMDgyODY3Mg.GV18-r.5lAUicaTU0pmdXsWsiCAfqi1CvtE9wEl81QM54",  // bot token gir :)
     PREFIX: process.env.PREFIX || "!", // bot prefix
-    SAHİP: process.env.SAHİP || "SAHİP İD", //sahip id :()
+    SAHİP: process.env.SAHİP || "772874961043718194", //sahip id :()
 }
